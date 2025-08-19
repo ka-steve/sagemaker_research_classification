15d5105d6d11a21eabf3ea960fd22695613f0c0f
Steve Attila Kopias <attila.steve.kopias@gmail.com>
2025-08-19 12:32:08 +0000

    Lifecycle config 008 autoexport

---
+++ src/00_system/lifecycle_config_exports/pip-install-008__OnStart.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# pip-install-008
+
+set -e
+
+echo "XXXXXX start_notebook START XXXXXXXXXXXXXXXXXX"
+
+sudo -u ec2-user -i <<EOF
+
+echo ":::::: start_notebook SUDO 1 START ::::::::::::::::::::::::"
+
+echo ":: source /home/ec2-user/anaconda3/bin/activate /home/ec2-user/anaconda3/envs/pytorch_p310"
+source /home/ec2-user/anaconda3/bin/activate /home/ec2-user/anaconda3/envs/pytorch_p310
+
+echo ":: pip install -r /home/ec2-user/SageMaker/research_methodology_extraction/requirements.txt"
+pip install -r /home/ec2-user/SageMaker/research_methodology_extraction/requirements.txt
+
+echo ":: cd /home/ec2-user/SageMaker/research_methodology_extraction/src/00_system/ && ls && pwd && python on_start.py"
+cd /home/ec2-user/SageMaker/research_methodology_extraction/src/00_system/ && ls && pwd && python on_start.py
+
+echo ":: conda deactivate"
+conda deactivate
+
+echo ":::::: start_notebook SUDO 1 END ::::::::::::::::::::::::"
+
+EOF
+
+sudo -u ec2-user -i <<EOF
+
+echo ":::::: start_notebook SUDO 2 START ::::::::::::::::::::::::"
+echo ":: source /home/ec2-user/anaconda3/bin/activate /home/ec2-user/anaconda3/envs/JupyterSystemEnv"
+source /home/ec2-user/anaconda3/bin/activate /home/ec2-user/anaconda3/envs/JupyterSystemEnv
+
+echo ":: pip install jupytext --upgrade"
+pip install jupytext --upgrade
+
+
+echo ":: sudo systemctl restart jupyter-server"
+sudo systemctl restart jupyter-server
+echo ":::::: start_notebook SUDO 2 END ::::::::::::::::::::::::"
+
+EOF
+
+echo "XXXXXX start_notebook END XXXXXXXXXXXXXXXXXX"
