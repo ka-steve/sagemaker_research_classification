8d5c40f15eaca5216b17e944b0e5249508a4dc83
ka-steve <attila.steve.kopias@gmail.com>
2025-08-29 17:04:37 +0000

    created base_semanticscholar_s2orcv2 and further exploration

---
+++ src/04_transformation/sql/02_stg__base_arxiv_metadata.sql
@@ -0,0 +1,30 @@
+WITH
+raw_arxiv_metadata AS 
+(
+    SELECT * FROM "01_raw".arxiv_metadatada -- mind the typo, fixed in downstream models
+),
+base_arxiv_metadata AS 
+(
+    SELECT
+        id AS arxiv_id, 
+        doi AS doi_id,
+        title,
+        abstract,
+        -- Converting license definitions to the same format as SemanticScholar
+        CASE
+            WHEN "license" IS NULL THEN NULL
+            WHEN "license" = 'http://creativecommons.org/licenses/by-nc-sa/4.0/' THEN 'CCBYNCSA'
+            WHEN "license" = 'http://creativecommons.org/publicdomain/zero/1.0/' THEN 'CC0'
+            WHEN "license" = 'http://creativecommons.org/licenses/by-nc-nd/4.0/' THEN 'CCBYNCND'
+            WHEN "license" = 'http://creativecommons.org/licenses/by/4.0/' THEN 'CCBY'
+            WHEN "license" = 'http://creativecommons.org/licenses/by-sa/4.0/' THEN 'CCBYSA'
+            WHEN "license" = 'http://creativecommons.org/licenses/by/3.0/' THEN 'CCBY'
+            WHEN "license" = 'http://creativecommons.org/licenses/by-nc-sa/3.0/' THEN 'CCBYNCSA'
+            WHEN "license" = 'http://creativecommons.org/licenses/publicdomain/' THEN 'public-domain'
+            WHEN "license" = 'http://arxiv.org/licenses/nonexclusive-distrib/1.0/' THEN 'ArXiv nonexclusive-distrib'
+            ELSE CONCAT('ArXiv: ', "license")
+        END AS license
+    FROM
+        raw_arxiv_metadata
+)
+SELECT * FROM base_arxiv_metadata
