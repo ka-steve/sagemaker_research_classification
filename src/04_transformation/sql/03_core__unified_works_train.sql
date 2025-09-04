0a5d363b37fd8e505b4d8f52d637d38a2bdd7888
ka-steve <attila.steve.kopias@gmail.com>
2025-09-04 00:48:15 +0000

    Started to upload data to HF (rate limited though)

---
+++ src/04_transformation/sql/03_core__unified_works_train.sql
@@ -0,0 +1,14 @@
+WITH
+-- Base models refreshed with LOWER DOI
+core_unified_works AS (
+    SELECT * FROM "03_core".unified_works
+),
+core_unified_works_subset AS (
+    SELECT
+        *
+    FROM
+        core_unified_works
+    WHERE
+        subset = 'train'
+)
+SELECT * FROM core_unified_works_subset
